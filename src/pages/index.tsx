6d2bb9784db268f25d40f14705e4d97304a4f006
saadtechgeek <saad.techgeek@gmail.com>
2021-01-16 11:39:11 +0500

    changes in form added formik

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
+import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
 
 export default function Home() {
   return (
     <Card>
       <CardContent>
-        <Typography variant="h1">Hello Youtube!!!</Typography>
+        <Formik initialValues={{
+            firstName: '',
+            lastName: '',
+            millionaire: false,
+            money: 0,
+            description: '',
+          }}
+        onSubmit={() => {}}>
+          <Form>
+            <Field name="firstName" component={TextField} label="First Name" />
+          </Form>
+        </Formik>
       </CardContent>
     </Card>
   );
