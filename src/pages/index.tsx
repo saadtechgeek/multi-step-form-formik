0068a4408d711c148ddbef3990714722bb09f903
saadtechgeek <saad.techgeek@gmail.com>
2021-01-16 21:20:38 +0500

    fix the issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
 import { Field, Form, Formik, FormikConfig, FormikValues } from 'formik';
 import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+import { mixed, number, object } from 'yup';
+import React, { useState } from 'react';
 
 export default function Home() {
   return (
     <Card>
       <CardContent>
-        <Formik initialValues={{
+        <FormikStepper
+          validationSchema={object({
+            money: mixed().when('millionaire', {
+              is: true,
+              then: number()
+                .required()
+                .min(
+                  1_000_000,
+                  'Because you said you are a millionaire you need to have 1 million'
+                ),
+              otherwise: number().required(),
+            }),
+          })}
+        
+        initialValues={{
             firstName: '',
             lastName: '',
             millionaire: false,
@@ -14,11 +30,32 @@ export default function Home() {
             description: '',
           }}
         onSubmit={() => {}}>
-          <Form>
-            <Field name="firstName" component={TextField} label="First Name" />
-          </Form>
-        </Formik>
+            <div>
+              <Field name="firstName" component={TextField} label="First Name" />
+              <Field name="lastName" component={TextField} label="Last Name" />
+              <Field name="millionaire" type="checkbox" component={CheckboxWithLabel} Label={{ label: 'I am a millionaire' }} />
+            </div>
+            <div>
+              <Field name="money" type="number" component={TextField} label="All the money I have" />
+            </div>
+            <div>
+              <Field name="description" component={TextField} label="Description" />
+            </div>
+        </FormikStepper>
       </CardContent>
     </Card>
   );
+}
+
+
+export function FormikStepper({children, ...props}: FormikConfig<FormikValues>){
+  const childrenArray = React.Children.toArray(children);
+  const [step, setStep] = useState(0);
+  const currentChild = childrenArray[step];
+return (
+  <Formik {...props}>
+       <Form autoComplete="off">{currentChild}</Form>
+  </Formik>
+);
+
 }
